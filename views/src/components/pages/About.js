ff812eb014699d2c0e20f081b20891d89d04ddf8
barathrajd <barathdeva757@gmail.com>
2020-08-01 15:48:35 +0530

    [Barathraj Gokulnath] About Page layout is change from grid to flex

--- views/src/components/pages/About.js
+++ views/src/components/pages/About.js
@@ -2,20 +2,24 @@ import React, { useContext } from 'react';
 import Upcoming from '../card/upCard';
 import Completed from '../card/compCard';
 import EventContext from '../../context/event/eventContext';
+import UpImg from '../../img/Mask2.png';
+import CompImg from '../../img/Mask3.png';
 
 const About = () => {
   const eventContext = useContext(EventContext);
 
   return (
     <div className='about'>
+      {/* HEADING SECTION */}
+
+      <h2>About Events</h2>
+
+      {/* ABOUT CONTENT */}
+
       <div className='section-about'>
-        {/* HEADING SECTION */}
-
-        <h2>About Events</h2>
-
         {/*  UPCOMING LEFT CARDS */}
 
-        <div className='upLeft-card'>
+        <div className='upLeft'>
           {/* UPCOMING LEFT TITLE */}
           <div className='title upLeft-title'>Upcoming Events</div>
 
@@ -39,7 +43,7 @@ const About = () => {
 
         {/* UPCOMING RIGHT SECTION */}
         <div className='upRight'>
-          <div alt='Demo' className='upRight-img' />
+          <img alt='Demo' src={UpImg} />
         </div>
       </div>
 
@@ -48,7 +52,7 @@ const About = () => {
         {/*  UPCOMING LEFT CARDS */}
 
         <div className='upRight'>
-          <div alt='Demo' className='compLeft-img' />
+          <img alt='Demo' src={CompImg} />
         </div>
 
         {/* UPCOMING RIGHT SECTION */}
@@ -80,46 +84,3 @@ const About = () => {
 };
 
 export default About;
-
-// import React from 'react';
-// import Card from '../card/Card';
-
-// const About = () => {
-//   return (
-//     <div className='about'>
-//       <br />
-//       <h2 className='heading-about'>About Events</h2>
-//       <div className='row'>
-//         <div className='upcoming col-7'>
-//           <div className='upcoming-content'>
-//             <div className='upcoming-title'>Upcoming Events</div>
-//             <div className='upcoming-card'>
-//               <div className='card-1'>
-//                 <Card />
-//               </div>
-//               <div className='card-2'>
-//                 <Card />
-//               </div>
-//               <div className='card-3'>
-//                 <Card />
-//               </div>
-//               <div className='card-4'>
-//                 <div className='cus-button'>
-//                   <span className='cus-button-text'>Load More</span>
-//                   <span className='cus-button-rarrow'>
-//                     <i className='fa fa-arrow-circle-right' />
-//                   </span>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div className='col-5'>
-//           <div alt='Demo' className='header-image-1' />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default About;
