53ebae2ec51cb2507beb669beffb2679a09c2e26
barathrajd <barathdeva757@gmail.com>
2020-08-05 20:12:25 +0530

    [Barathraj Sudhira Aadish] Signup Form Designed

--- views/src/App.js
+++ views/src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import Home from "./components/pages/Home";
-import Navbar from "./components/layout/Navbar";
-import EventState from "../src/context/event/eventState";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import Navbar from './components/layout/Navbar';
+import EventState from '../src/context/event/eventState';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap";
-import "jquery/dist/jquery.slim";
-import "popper.js";
-import Upcomingindiv from "./components/card/UpcomingCardItem";
-import Completedindiv from "./components/card/CompletedCardItem";
-import UpcomLoadPage from "./components/pages/UpcomLoadPage";
-import CompLoadPage from "./components/pages/CompLoadPage";
-import Signup from "./components/auth/Signup";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import 'jquery/dist/jquery.slim';
+import 'popper.js';
+import Upcomingindiv from './components/card/UpcomingCardItem';
+import Completedindiv from './components/card/CompletedCardItem';
+import UpcomLoadPage from './components/pages/UpcomLoadPage';
+import CompLoadPage from './components/pages/CompLoadPage';
+import Signup from './components/auth/Signup';
 
 function App() {
   return (
@@ -21,12 +21,12 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={Signup} />
-          {/*<Route exact path='/' component={Home} />*/}
-          <Route exact path="/upcomingEvents" component={Upcomingindiv} />
-          <Route exact path="/completedEvents" component={Completedindiv} />
-          <Route exact path="/upcoming-list" component={UpcomLoadPage} />
-          <Route exact path="/completed-list" component={CompLoadPage} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/upcomingEvents' component={Upcomingindiv} />
+          <Route exact path='/completedEvents' component={Completedindiv} />
+          <Route exact path='/upcoming-list' component={UpcomLoadPage} />
+          <Route exact path='/completed-list' component={CompLoadPage} />
         </Switch>
       </Router>
     </EventState>
--- views/src/components/auth/Signup.js
+++ views/src/components/auth/Signup.js
@@ -1,29 +1,91 @@
-import React from "react";
+import React from 'react';
+import signup from '../../img/signup.jpg';
 
 const Signup = () => {
   return (
-    <div className="signup">
-      <form>
-        <h1>Registration Form</h1>
-        <div className="form_group">
-          <label htmlFor="email">E-mail: </label>
-          <input
-            className="form_input"
-            type="text"
-            name="email"
-            placeholder="Email"
-          />
-        </div>
-        <div className="form_group">
-          <label htmlFor="password">Password:</label>
-          <input
-            className="form_input"
-            type="text"
-            name="password"
-            placeholder="Password"
-          />
-        </div>
-      </form>
+    <div className='bg-signup'>
+      <div className='signup'>
+        <img src={signup} alt='signup' className='imgLeft' />
+        <form className='form-signup'>
+          <h1>Sign Up</h1>
+          <span className='inputs'>
+            <div className='form_group'>
+              <label htmlFor='firstname'>First Name</label>
+              <input
+                className='form_input'
+                type='text'
+                name='firstname'
+                id='firstname'
+                placeholder='Firstname'
+              />
+            </div>
+            <div className='form_group'>
+              <label htmlFor='lastname'>Last Name</label>
+              <input
+                className='form_input'
+                type='text'
+                name='lastname'
+                id='lastname'
+                placeholder='Lastname'
+              />
+            </div>
+            <div className='form_group'>
+              <label htmlFor='username'>Username</label>
+              <input
+                className='form_input'
+                type='text'
+                name='username'
+                id='username'
+                placeholder='Username'
+              />
+            </div>
+            <div className='form_group'>
+              <label htmlFor='email'>Email Address</label>
+              <input
+                className='form_input'
+                type='text'
+                name='email'
+                placeholder='Enter your email'
+              />
+            </div>
+            <div className='form_group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                className='form_input'
+                type='password'
+                name='password'
+                id='password'
+                placeholder='Enter your Password'
+              />
+            </div>
+            <div className='form_group'>
+              <label htmlFor='cpassword'>Confirm Password</label>
+              <input
+                className='form_input'
+                type='password'
+                name='password'
+                id='cpassword'
+                placeholder='Re-enter your Password'
+              />
+            </div>
+            <div className='form_group'>
+              <label htmlFor='dob'>Date of Birth</label>
+              <input type='date' name='date' id='dob' className='form_input' />
+            </div>
+            <div className='form_group'>
+              <label htmlFor='gender'>Gender</label>
+              <span className=''>
+                <input type='radio' id='gender' name='gender' />
+                Male
+                <input type='radio' id='gender' name='gender' />
+                Female
+                <input type='radio' id='gender' name='gender' />
+                Others
+              </span>
+            </div>
+          </span>
+        </form>
+      </div>
     </div>
   );
 };
