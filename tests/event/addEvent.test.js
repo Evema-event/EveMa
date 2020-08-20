ab9703eaa24e2eb3d4d015fc7bc3587ea2c9b746
Gokulnath <4gokul2000@gmail.com>
2020-08-20 12:03:57 +0530

    Refactored event api test cases
    Co-authored-by: NITHIYASRI <nithiyasri@users.noreply.github.com>

--- tests/event/addEvent.test.js
+++ tests/event/addEvent.test.js
@@ -2,44 +2,27 @@
 const request = require('supertest');
 
 // Importing app and db
-const app = require('../app');
-const db = require('../db');
+const app = require('../../app');
+const db = require('../../db');
 
 // Importing db models
-const Event = require('../models/event');
-const User = require('../models/user');
+const Event = require('../../models/event');
+const User = require('../../models/user');
 
 // Importing Dummy data
-const events = require('./dummyData/event');
-const users = require('./dummyData/signup');
+const events = require('../dummyData/event');
 
 // Importing utility functions
-const hashPassword = require('../utility/hashPassword');
-const tokenGenerator = require('../utility/tokenGenerator');
+const getToken = require('../utility/getToken');
 
 // Global variables
-let organizerToken;
-let visitorToken;
-let exhibitorToken;
+let token;
 
 // Run before all test cases - will connect to data base and create two events
 // One upcoming event and one completed event
 beforeAll(async () => {
-    await db.connectDB('test-event');
-    await Event.create(events.valid);
-    const organizer = await new User({
-        ...users.organizer,
-        password: await hashPassword(users.organizer.password)
-    }).save();
-    organizerToken = await tokenGenerator({
-        userId: organizer._id,
-        emailId: organizer.emailId,
-        userName: organizer.userName
-    });
-    const visitor = await request(app).post('/api/user/signup').send(users.visitor);
-    visitorToken = visitor.body.token;
-    const exhibitor = await request(app).post('/api/user/signup').send(users.exhibitor);
-    exhibitorToken = exhibitor.body.token;
+    await db.connectDB('test-addevent');
+    token = await getToken();
 });
 
 // Run after all test cases finished 
@@ -48,26 +31,6 @@ afterAll(async () => {
     await Event.deleteMany();
     await User.deleteMany();
     await db.disconnectDB();
-});
-
-// All test cases are listed below
-describe('Get events from database', () => {
-
-    // Test upcoming event is only one
-    test('Get upcomingEvents', async () => {
-        const res = await request(app).get('/api/event/upcomingEvents');
-        expect(res.status).toBe(200);
-        expect(res.body.events.length).toBe(1);
-        expect(res.body.events[0].name).toBe('Test 2');
-    });
-
-    // Test completed event is only one
-    test('Get completedEvents', async () => {
-        const res = await request(app).get('/api/event/completedEvents');
-        expect(res.status).toBe(200);
-        expect(res.body.events.length).toBe(1);
-        expect(res.body.events[0].name).toBe('Test 1');
-    });
 });
 
 // Test cases for add event
@@ -97,7 +60,7 @@ describe('Add Event to database', () => {
     test('Post Add event with visitor token', async () => {
         const res = await request(app)
             .post('/api/event/addEvent')
-            .set('x-auth-token', visitorToken)
+            .set('x-auth-token', token.visitorToken)
             .send(events.valid[1]);
         expect(res.status).toBe(401);
         expect(res.body.message).toBe('Failed');
@@ -107,7 +70,7 @@ describe('Add Event to database', () => {
     test('Post Add event with exhibitor token', async () => {
         const res = await request(app)
             .post('/api/event/addEvent')
-            .set('x-auth-token', exhibitorToken)
+            .set('x-auth-token', token.exhibitorToken)
             .send(events.valid[1]);
         expect(res.status).toBe(401);
         expect(res.body.message).toBe('Failed');
@@ -117,7 +80,7 @@ describe('Add Event to database', () => {
     test('Post Add event with invalid event data', async () => {
         const res = await request(app)
             .post('/api/event/addEvent')
-            .set('x-auth-token', organizerToken)
+            .set('x-auth-token', token.organizerToken)
             .send(events.invalid[0]);
         expect(res.status).toBe(422);
         expect(res.body.error.length).toBe(11);
@@ -128,7 +91,7 @@ describe('Add Event to database', () => {
     test('Post Add event with valid data', async () => {
         const res = await request(app)
             .post('/api/event/addEvent')
-            .set('x-auth-token', organizerToken)
+            .set('x-auth-token', token.organizerToken)
             .send(events.valid[1]);
         expect(res.status).toBe(200);
         expect(res.body.message).toBe('Success');
