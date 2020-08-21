95827101f0e58f5a9dc91d13a478a1088ef3fc8f
barathrajd <barathdeva757@gmail.com>
2020-08-21 16:44:14 +0530

    [Barathraj Sudhira] Admin Login and Button CSS Updated

--- views/src/context/auth/authState.js
+++ views/src/context/auth/authState.js
@@ -47,7 +47,7 @@ const AuthState = (props) => {
 
   const authentication = (response) => {
     let user;
-    if (response.data.user.role == 'Organizer') {
+    if (response.data.user.role === 'Organizer') {
       user = {
         userId: response.data.user._id,
         username: response.data.user.userName,
