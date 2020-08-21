422158d0f0f5e79fd0421d8383c39bbd2810c76c
Gokulnath <4gokul2000@gmail.com>
2020-08-21 16:33:04 +0530

    Organizer login error fixed

--- views/src/context/auth/authState.js
+++ views/src/context/auth/authState.js
@@ -46,29 +46,39 @@ const AuthState = (props) => {
   };
 
   const authentication = (response) => {
-    const user = {
-      userId: response.data.user._id,
-      username: response.data.user.userName,
-      password: response.data.user.password,
-      email: response.data.user.emailId,
-      role: '',
-      firstname: response.data.profile.firstName,
-      lastname: response.data.profile.lastName,
-      state: response.data.profile.state,
-      country: response.data.profile.country,
-      zipcode: response.data.profile.zipCode,
-      city: response.data.profile.cityName,
-      dob: response.data.profile.dateOfBirth,
-      gender: response.data.profile.gender,
-      destination: response.data.profile.designation,
-      areasOfInterest: response.data.profile.areaOfInterest,
-      company: response.data.profile.companyName,
-      contact: response.data.profile.contactNumber,
-      address: response.data.profile.companyAddress,
-      registeredEvents: response.data.profile.registeredEvents,
-      registeredStalls: response.data.profile.registeredStalls,
-    };
-
+    let user;
+    if (response.data.user.role == 'Organizer') {
+      user = {
+        userId: response.data.user._id,
+        username: response.data.user.userName,
+        password: response.data.user.password,
+        email: response.data.user.emailId,
+        role: '',
+      };
+    } else {
+      user = {
+        userId: response.data.user._id,
+        username: response.data.user.userName,
+        password: response.data.user.password,
+        email: response.data.user.emailId,
+        role: '',
+        firstname: response.data.profile.firstName,
+        lastname: response.data.profile.lastName,
+        state: response.data.profile.state,
+        country: response.data.profile.country,
+        zipcode: response.data.profile.zipCode,
+        city: response.data.profile.cityName,
+        dob: response.data.profile.dateOfBirth,
+        gender: response.data.profile.gender,
+        destination: response.data.profile.designation,
+        areasOfInterest: response.data.profile.areaOfInterest,
+        company: response.data.profile.companyName,
+        contact: response.data.profile.contactNumber,
+        address: response.data.profile.companyAddress,
+        registeredEvents: response.data.profile.registeredEvents,
+        registeredStalls: response.data.profile.registeredStalls,
+      };
+    }
     if (response.data.user.role.length === 1) {
       user.role = response.data.user.role[0];
     } else {
