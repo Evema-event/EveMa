d94ef9fddb62521934ed0b348f8e5aaa0855fe27
barathrajd <barathdeva757@gmail.com>
2020-08-27 15:15:33 +0530

    Redesign the Stall List
    Co-authored-by: aadish-n <aadish-n@users.noreply.github.com>
    Co-authored-by: poovarasansp <poovarasansp@users.noreply.github.com>

--- views/src/components/card/stallList.js
+++ views/src/components/card/stallList.js
@@ -1,36 +1,25 @@
 import React from 'react';
 
 const stallList = () => {
-  return(
-      <div>
-         <div className='stall-bg'>
-           <div className='product-name'>
-            <h5>Product Name</h5>
-            <div className='product-bg product-color'>
-             The Terminator
-            </div>
-           </div>
-         <div className='product-name'>
-         <h5>Company</h5>
-         <div className='product-field product-color'>
-          Genisys
-         </div>
+  return (
+    <div>
+      <div className='stall-bg'>
+        <div className='stall-title'>Terminator</div>
+        <div className='product-name'>
+          <h5>Company</h5>
+          <h6>Genisys</h6>
         </div>
         <div className='product-name'>
-         <h5>Domain</h5>
-         <div className='product-value product-color'>
-          A.I Robotics
-         </div>
+          <h5>Domain</h5>
+          <h6>A.I Robotics</h6>
         </div>
-        <div className='product-desc'>
-         <h5>Description</h5>
-         <div className='product-description product-color'>
-         The Robot that is here to save Sarah Connor thereby saving the human race.
-         </div>
+        <div className='product-name'>
+          <h5>Email</h5>
+          <h6> demo@tect.com </h6>
         </div>
-       </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default stallList;
--- views/src/components/pages/stallView.js
+++ views/src/components/pages/stallView.js
@@ -1,12 +1,60 @@
 import StallList from '../card/stallList';
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const stallView = () => {
   return (
     <div>
-      <StallList />
+      <>
+        <ul className='nav nav-pills  nav-justified' id='myTab' role='tablist'>
+          <li className='nav-item'>
+            <Link
+              className='nav-link active'
+              id='stall-tab'
+              data-toggle='tab'
+              role='tab'
+              aria-controls='stall'
+              aria-selected='true'
+              to='/stallList'
+            >
+              Stall
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              className='nav-item nav-link'
+              id='conference-tab'
+              data-toggle='tab'
+              role='tab'
+              aria-controls='conference'
+              aria-selected='false'
+              to='/conferenceList'
+            >
+              Conference
+            </Link>
+          </li>
+        </ul>
+      </>
+      <div className='tab-content' id='myTabContent'>
+        <div
+          className='tab-pane active'
+          id='stall'
+          role='tabpanel'
+          aria-labelledby='stall-tab'
+        >
+          <StallList />
+        </div>
+        <div
+          className='tab-pane'
+          id='conference'
+          role='tabpanel'
+          aria-labelledby='conference-tab'
+        >
+          <StallList />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default stallView
+export default stallView;
