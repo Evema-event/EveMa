0e291f5276963a905c1ea0483daf163b019c9de9
GokulnathP <4gokul2000@gmail.com>
2020-09-02 18:11:46 +0530

    Conference context state added
    Co-authored-by: NITHIYASRI <nithiyasri@users.noreply.github.com>
    Co-authored-by: Aadish-N <aadish-n@users.noreply.github.com>
    Co-authored-by: barathrajd <barathrajd@users.noreply.github.com>

--- views/src/components/card/conferenceList.js
+++ views/src/components/card/conferenceList.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import ConferenceContext from '../../context/conference/conferenceContext';
 
-const ConferenceList = () => {
+const ConferenceList = (props) => {
+  const { setIndividualConference } = useContext(ConferenceContext);
+
+  const onClickConference = () => {
+    setIndividualConference(props.conference);
+  }
+
   return (
-    <Link to='/stallIndividual'>
-      <div>
-        <div className='stall-bg'>
-          <div className='stall-title'>Terminator</div>
-          <div className='product-name'>
-            <h5>Company</h5>
-            <h6>Genisys</h6>
-          </div>
-          <div className='product-name'>
-            <h5>Domain</h5>
-            <h6>A.I Robotics</h6>
-          </div>
-          <div className='product-name'>
-            <h5>Email</h5>
-            <h6> johnconnor@genisys.tech </h6>
-          </div>
+    <div onClick={onClickConference}>
+      <div className='stall-bg'>
+        <div className='stall-title'>{props.conference.title}</div>
+        <div className='product-name'>
+          <h5>Company</h5>
+          <h6>{props.conference.user.companyName}</h6>
+        </div>
+        <div className='product-name'>
+          <h5>Domain</h5>
+          <h6>{props.conference.theme}</h6>
+        </div>
+        <div className='product-name'>
+          <h5>Email</h5>
+          <h6>{props.conference.userId.emailId}</h6>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
