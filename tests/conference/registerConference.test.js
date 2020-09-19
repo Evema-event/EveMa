85ce47253b7f2b1ca3dcb4b76f2dc920e18e7eea
GokulnathP <4gokul2000@gmail.com>
2020-09-19 18:05:31 +0530

    register conferences api test cases added

--- tests/conference/registerConference.test.js
+++ tests/conference/registerConference.test.js
@@ -8,11 +8,11 @@ const db = require('../../db');
 // Importing db models
 const User = require('../../models/user');
 const Event = require('../../models/event');
-const Stall = require('../../models/stall');
+const Conference = require('../../models/conference');
 const Profile = require('../../models/profile');
 
 // Importing dumyData
-const stallData = require('../dummyData/stall');
+const conferenceData = require('../dummyData/conference');
 const eventData = require('../dummyData/event');
 
 // Importing utility functions for testing
@@ -24,7 +24,7 @@ let eventId;
 
 // Run before all test cases - will create users and get token for each
 beforeAll(async () => {
-    await db.connectDB('test-registerstall');
+    await db.connectDB('test-registerconference');
     token = await getToken();
     const eventRes = await new Event(eventData.valid[1]).save();
     eventId = eventRes._id;
@@ -34,17 +34,17 @@ beforeAll(async () => {
 afterAll(async () => {
     await User.deleteMany();
     await Event.deleteMany();
-    await Stall.deleteMany();
+    await Conference.deleteMany();
     await Profile.deleteMany();
     await db.disconnectDB();
 });
 
-// Test cases for register stall
-describe('test cases for register stall', () => {
+// Test cases for register conference
+describe('test cases for register conference', () => {
 
     // Token must be present
     test('should throw error if token not present', async () => {
-        const res = await request(app).post('/api/stall/registerStall/5f44ce10f0ff6424c008ce0d');
+        const res = await request(app).post('/api/conference/registerConference/5f44ce10f0ff6424c008ce0d');
         expect(res.status).toBe(401);
         expect(res.body.message).toBe("Failed");
     });
@@ -52,39 +52,39 @@ describe('test cases for register stall', () => {
     // Error should be return for invalid data
     test('should throw error for invalid data', async () => {
         const res = await request(app)
-            .post('/api/stall/registerStall/5f44ce10f0ff6424c008ce0d')
+            .post('/api/conference/registerConference/5f44ce10f0ff6424c008ce0d')
             .set('x-auth-token', token.exhibitorToken)
-            .send(stallData.invalid);
+            .send(conferenceData.invalid);
         expect(res.status).toBe(422);
-        expect(res.body.error.length).toBe(3);
+        expect(res.body.error.length).toBe(7);
     });
 
     // Throw error for invalid event
     test('should throw error for invalid event id', async () => {
         const res = await request(app)
-            .post('/api/stall/registerStall/5f44ce10f0ff6424c008ce0d')
+            .post('/api/conference/registerConference/5f44ce10f0ff6424c008ce0d')
             .set('x-auth-token', token.exhibitorToken)
-            .send(stallData.valid);
+            .send(conferenceData.valid);
         expect(res.status).toBe(404);
         expect(res.body.message).toBe("Failed");
     });
 
-    // Visitor cannot register stall
+    // Visitor cannot register conference
     test('should throw error for visitor token', async () => {
         const res = await request(app)
-            .post('/api/stall/registerStall/' + eventId)
+            .post('/api/conference/registerConference/' + eventId)
             .set('x-auth-token', token.visitorToken)
-            .send(stallData.valid);
+            .send(conferenceData.valid);
         expect(res.status).toBe(401);
         expect(res.body.message).toBe("Failed");
     });
 
-    // Organizer cannot register stall
+    // Organizer cannot register conference
     test('should throw error for organizer token', async () => {
         const res = await request(app)
-            .post('/api/stall/registerStall/' + eventId)
+            .post('/api/conference/registerConference/' + eventId)
             .set('x-auth-token', token.organizerToken)
-            .send(stallData.valid);
+            .send(conferenceData.valid);
         expect(res.status).toBe(401);
         expect(res.body.message).toBe("Failed");
     });
@@ -92,24 +92,20 @@ describe('test cases for register stall', () => {
     // Able to register as exhibitor for valid data 
     test('should return success for valid data', async () => {
         const res = await request(app)
-            .post('/api/stall/registerStall/' + eventId)
+            .post('/api/conference/registerConference/' + eventId)
             .set('x-auth-token', token.exhibitorToken)
-            .send(stallData.valid);
+            .send(conferenceData.valid);
         expect(res.status).toBe(200);
         expect(res.body.message).toBe("Success");
-        expect(res.body.stall.productName).toBe(stallData.valid.productName);
+        expect(res.body.conference.title).toBe(conferenceData.valid.title);
     });
 
-    // Throw error for try register more than 2 stall
-    test('should throw error for more than 2 stall', async () => {
+    // Throw error for try register more than 1 conference
+    test('should throw error for more than 1 confernce register', async () => {
         let res = await request(app)
-            .post('/api/stall/registerStall/' + eventId)
+            .post('/api/conference/registerConference/' + eventId)
             .set('x-auth-token', token.exhibitorToken)
-            .send(stallData.valid);
-        res = await request(app)
-            .post('/api/stall/registerStall/' + eventId)
-            .set('x-auth-token', token.exhibitorToken)
-            .send(stallData.valid);
+            .send(conferenceData.valid);
         expect(res.status).toBe(422);
         expect(res.body.message).toBe("Failed");
     });
