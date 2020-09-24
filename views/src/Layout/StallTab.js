fd229e455535751f7c9abdcf53477830d69fbce8
GokulnathP <4gokul2000@gmail.com>
2020-09-24 11:54:30 +0530

    Visitor details bug resolved
    Co-authored-by: Sudhira13 <sudhira13@users.noreply.github.com>

--- views/src/Layout/StallTab.js
+++ views/src/Layout/StallTab.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import AuthContext from '../context/auth/authContext';
+import StallContext from '../context/stall/stallContext';
+
 const StallTab = (props) => {
+    const authContext = useContext(AuthContext);
+    const { individualStall } = useContext(StallContext);
     return (
         <ul className="nav nav-tabs" style={{ margin: "10px 50px 0px 50px" }}>
             <li className="nav-item">
                 <Link className={props.tab === "stall" ? "nav-link active" : "nav-link"} to='/stallDetails'>Stall Details</Link>
             </li>
             {
-                (localStorage.getItem('role') === 'Organizer' || localStorage.getItem('role') === 'Exhibitor') &&
+                (localStorage.getItem('role') === 'Organizer' || (localStorage.getItem('role') === 'Exhibitor' && individualStall.userId._id === authContext.userId)) &&
                 <li className="nav-item">
                     <Link to='/stallvisitorList' className={props.tab === "visitor" ? "nav-link active" : "nav-link"}>
                         Visitors
