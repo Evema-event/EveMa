d010f347c00096fd5d923819a8f0bf5524a30da5
sudhira <sudhira.17it@kct.ac.in>
2020-09-26 15:37:17 +0530

    Change password test cases added
    Co-authored-by: GokulnathP <gokulnathp@users.noreply.github.com>

---
+++ tests/dummyData/changePassword.js
@@ -0,0 +1,14 @@
+module.exports = {
+    valid: {
+        oldPassword: 'visitor',
+        newPassword: '12345678'
+    },
+    invalid: {
+        oldPassword: 'vis',
+        newPassword: '123'
+    },
+    invalidPassword: {
+        oldPassword: 'exhibitor',
+        newPassword: '12345678'
+    }
+};
